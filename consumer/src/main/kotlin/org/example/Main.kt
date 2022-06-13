89397482dd88c474c3ca3b547f49640d948630bd
nikolaDrljaca <devnikoladr@gmail.com>
2022-06-13 15:33:00 +0300

    Consumed messages are now saved to db.

--- consumer/src/main/kotlin/org/example/Main.kt
+++ consumer/src/main/kotlin/org/example/Main.kt
@@ -1,35 +1,11 @@
 package org.example
 
+import com.rabbitmq.client.ConnectionFactory
 import org.example.dao.DatabaseFactory
 import org.example.dao.messageDao
 
 suspend fun main() {
     DatabaseFactory.init()
-
-    println("Executing SQL statements.")
-
-    val allMessages = messageDao.getAllMessages()
-    println("Message table content: $allMessages")
-
-    println("Inserting messages.")
-    val message = messageDao.insert("This is the first message")
-    messageDao.apply {
-        insert("Second after first")
-        insert("foo bar")
-        insert("bazz bazz")
-    }
-
-    println("Message table content after insertion ${messageDao.getAllMessages()}")
-
-    message?.let { messageDao.deleteMessage(it.id) }
-
-    println("Message table content after deletion ${messageDao.getAllMessages()}")
-
-    val foo = messageDao.getAllMessages().first()
-
-    messageDao.updateMessage(foo.id, "This is updated content.")
-
-    /*
     val factory = ConnectionFactory().apply {
         username = "guest"
         password = "guest"
@@ -41,7 +17,7 @@ suspend fun main() {
 
     //don't close this in the consumer, keep it running to receive messages
     val connection = factory.newConnection()
-    println("Established connection")
+    println("Established connection!")
 
     val channel = connection.createChannel()
     channel.queueDeclare(
@@ -51,34 +27,23 @@ suspend fun main() {
         false, //autoDelete
         null //arguments
     )
-    println("Channel created")
+    println("Channel created!")
 
     channel.basicConsumeDeliveryFlow("hello_world", true)
         .collect {
             when(it) {
-                is ChannelHelper.Delivery -> {  }
-                is ChannelHelper.Cancel -> {  }
+                is ChannelHelper.Delivery -> {
+                    println("Received Message!")
+                    messageDao.insert(it.delivery.body.decodeToString())?.let { message ->
+                        println("Message saved! Content: $message")
+                    }
+                    println("---------------------------")
+                    println(messageDao.getAllMessages().joinToString(separator = "\n", limit = 20))
+                }
+                is ChannelHelper.Cancel -> {
+                    //Do nothing
+                }
             }
         }
-
-
-     */
-
-    /*
-    val deliveryCallback: (String, Delivery) -> Unit = { consumerTag, delivery ->
-        println("Received message: ${delivery.body.decodeToString()}")
-    }
-
-    val cancelCallback: (String) -> Unit = {
-
-    }
-
-    channel.basicConsume(
-        "hello_world", //queue name
-        true, //acknowledgement
-        deliveryCallback,
-        cancelCallback
-    )
-     */
 }
 
